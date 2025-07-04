56020c596400a7a3021b87be385416daf5cedc02
vincenzo.dev.82 <92805484+vincenzo-dev-82@users.noreply.github.com>
2025-07-04 19:02:18 +0900

    Change all services to use single 'shop' database instead of separate databases

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,13 +1,7 @@
--- Create databases for each service
-CREATE DATABASE IF NOT EXISTS member_db;
-CREATE DATABASE IF NOT EXISTS product_db;
-CREATE DATABASE IF NOT EXISTS order_db;
-CREATE DATABASE IF NOT EXISTS payment_db;
+-- Create single database for all services
+CREATE DATABASE IF NOT EXISTS shop;
 
 -- Grant privileges
-GRANT ALL PRIVILEGES ON member_db.* TO 'root'@'%';
-GRANT ALL PRIVILEGES ON product_db.* TO 'root'@'%';
-GRANT ALL PRIVILEGES ON order_db.* TO 'root'@'%';
-GRANT ALL PRIVILEGES ON payment_db.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON shop.* TO 'root'@'%';
 
 FLUSH PRIVILEGES;
